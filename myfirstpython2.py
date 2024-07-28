7ac36aec0f5cb8a31bed116ec3cb80f6d434a7a8
Turtle-Savant <nielsen.coleman2@gmail.com>
2024-07-28 15:54:52 +0000

    Inital commit

--- myfirstpython2.py
+++
@@ -1,7 +0,0 @@
-#this is a code written by coleman
-#it's not a great code, because that's not the point
-#i am trying ti learn linux
-
-for i in range(1,13):
-	print(str([j*i for j in range(1,13)]))
-
